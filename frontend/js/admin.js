fc1e5be473bb6b874dcf4153ffd7bb53af2b0f22
Diablo Drehmer <yagorsbarber@gmail.com>
2026-03-18 21:29:16 -0300

    corrigindo admin

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,22 +1,15 @@
 const API = window.location.origin
 
-if(localStorage.getItem("logado") !== "sim"){
-window.location.href = "login.html"
-}
-
 function carregarAgenda(){
-
-fetch(`${API}/agendamentos`)
-
-.then(res => res.json())
-
-.then(dados => {
 
 const dataFiltro = document.getElementById("filtroData").value
 const barbeiroFiltro = document.getElementById("filtroBarbeiro").value
 
-const lista = document.getElementById("lista")
+fetch(`${API}/agendamentos`)
+.then(res => res.json())
+.then(dados => {
 
+const lista = document.getElementById("lista")
 lista.innerHTML = ""
 
 dados.forEach(a => {
@@ -26,46 +19,34 @@ if(barbeiroFiltro && a.barbeiro != barbeiroFiltro) return
 
 const item = document.createElement("li")
 
-item.innerHTML =
-a.data + " - " + a.horario +
-" | " + a.nome +
-" | Barbeiro: " + a.barbeiro +
-` <button onclick="cancelar('${a.nome}','${a.data}','${a.horario}')">Cancelar</button>`
+item.innerHTML = `
+${a.data} - ${a.horario} |
+${a.nome} |
+Barbeiro: ${a.barbeiro}
+<button onclick="cancelar('${a.nome}','${a.data}','${a.horario}')">❌</button>
+`
 
 lista.appendChild(item)
 
 })
 
 })
+.catch(error => {
+console.log("Erro ao carregar agenda:", error)
+})
 
 }
 
-function cancelar(nome,data,horario){
+function cancelar(nome, data, horario){
 
 fetch(`${API}/cancelar`,{
-
 method:"POST",
-
-headers:{
-"Content-Type":"application/json"
-},
-
-body:JSON.stringify({
-nome:nome,
-data:data,
-horario:horario
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({nome, data, horario})
 })
-
-})
-
-.then(res => res.json())
-
-.then(()=>{
-
+.then(() => {
 alert("Agendamento cancelado")
-
 carregarAgenda()
-
 })
 
 }
